5918a22476cf9ae6ad199764a8c59e6a017ae7f5
Maskuerade <mariamahmedsaieed@gmail.com>
2023-07-12 20:51:52 +0300

    updated version of the webpage

--- script.js
+++ script.js
@@ -27,6 +27,34 @@ function bindData(articles) {
     });
 }
 
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+    const sortSelect = document.getElementById("sort-select");
+    const sortBy = sortSelect.value; // Get the selected sorting criteria
+
+    cardsContainer.innerHTML = "";
+
+    // Sort articles based on the selected criteria
+    articles.sort((a, b) => {
+        if (sortBy === "publishedAt") {
+            return new Date(b.publishedAt) - new Date(a.publishedAt);
+        } else if (sortBy === "title") {
+            return a.title.localeCompare(b.title);
+        } else if (sortBy === "source") {
+            return a.source.name.localeCompare(b.source.name);
+        }
+    });
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+
 function fillDataInCard(cardClone, article) {
     const newsImg = cardClone.querySelector('#news-img');
     const newsTitle = cardClone.querySelector('#news-title');
@@ -69,3 +97,29 @@ searchButton.addEventListener("click", () => {
     curSelectedNav?.classList.remove("active");
     curSelectedNav = null;
 });
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    let imgUrl = article.urlToImage;
+    let alt =
+        "https://pm1.aminoapps.com/8259/ad81db1d1e1e6804a726c03e07cfdfc4e4172ee4r1-368-349v2_uhq.jpg";
+    newsImg.src = imgUrl;
+    newsImg.onerror = function () {
+        newsImg.src = alt;
+    };
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Africa/Cairo",
+    });
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
